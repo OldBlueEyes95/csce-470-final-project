12b14265c5f5abbc1b2fa488de7a5c184525155d
Noah Batchelor <oldblueeyes@tamu.edu>
2024-10-21 15:58:00 -0500

    Basic Word Count Stats

--- src/data_stats.py
+++ src/data_stats.py
@@ -1,13 +1,40 @@
 import pandas as pd
-from typing import Tuple
+from typing import List, Tuple
 
 from serialize_data import retrieve_data
+
+STATS_FILE: str = "stats/basic_stats.txt"
 
 def main() -> None:
     title_frame, body_frame = retrieve_data()
     
-    print(title_frame.dtypes, body_frame.dtypes, sep='\n\n\n')
+    title_common_words: List[str, float] = [
+        (field, title_frame[field].sum())
+        for field in title_frame.columns
+    ]
+    title_common_words.sort(key=(lambda x: x[-1]))
     
+    
+    body_common_words: List[str, float] = [
+        (field, body_frame[field].sum())
+        for field in body_frame.columns
+    ]
+    body_common_words.sort(key=(lambda x: x[-1]))
+    
+    
+    # print(title_frame.dtypes, body_frame.dtypes, sep='\n\n\n')
+    with open(STATS_FILE, mode='w+', encoding='utf8') as stats_file:
+        # for field in title_frame.columns:
+            # stats_file.write(f"`{field}`: `{title_frame[field].mean()}`.\n")
+        stats_file.write("==== TITLE WORD COUNTS ====\n")
+        
+        for word, average in title_common_words:
+            stats_file.write(f"`{word}`: `{average}`.\n")
+        
+        stats_file.write("\n\n\n==== BODY WORD COUNTS ====\n")
+        
+        for word, average in body_common_words:
+            stats_file.write(f"`{word}`: `{average}`.\n")
 
 if __name__ == "__main__":
     main()
