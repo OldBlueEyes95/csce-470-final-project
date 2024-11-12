b929502b84fa8ede1939a5d7469ac0ab8936dcf8
Noah Batchelor <oldblueeyes@tamu.edu>
2024-11-11 20:20:38 -0600

    Updated env url

--- frontend/src/api/searchApi.js
+++ frontend/src/api/searchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || `https://${process.env.VERCEL_URL}` || 'http://localhost:5000';
 
 export async function fetchSearchResults(query) {
   console.log('Trying to request from:', `${BASE_URL}/search`);
