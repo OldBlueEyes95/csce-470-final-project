20ea52f050e4fbc3fd199bc2f32b3b3306ec2b21
Noah Batchelor <oldblueeyes@tamu.edu>
2024-11-11 21:07:04 -0600

    Back to env variables

--- frontend/src/api/searchApi.js
+++ frontend/src/api/searchApi.js
@@ -1,27 +1,29 @@
 import axios from 'axios';
 
-let BASE_URL = '';  // Initialize an empty URL
+const BASE_URL = process.env.REACT_APP_VERCEL_URL || 'http://localhost:5000';
 
 // Fetch the base URL dynamically from the Flask API
-async function fetchBaseUrl() {
-  try {
-    const response = await axios.get('/api/base_url');
-    console.log('Base URL response:', response);
-    BASE_URL = response.data.base_url;  // Set the base URL dynamically
-    console.log('Fetched Base URL:', BASE_URL);
-  } catch (error) {
-    console.error('Error fetching base URL:', error);
-    BASE_URL = 'http://localhost:5000';  // Fallback to localhost if error
-  }
-}
+// async function fetchBaseUrl() {
+//   try {
+//     const response = await axios.get('/api/base_url');
+//     console.log('Base URL response:', response);
+//     BASE_URL = response.data.base_url;  // Set the base URL dynamically
+//     console.log('Fetched Base URL:', BASE_URL);
+//   } catch (error) {
+//     console.error('Error fetching base URL:', error);
+//     BASE_URL = 'http://localhost:5000';  // Fallback to localhost if error
+//   }
+// }
 
 // Use the dynamic BASE_URL for your search requests
 export async function fetchSearchResults(query) {
   // Ensure the base URL is set before making a request
-  if (!BASE_URL) {
-    await fetchBaseUrl();  // Fetch the URL if not already set
-  }
-
+  // if (!BASE_URL) {
+  //   await fetchBaseUrl();  // Fetch the URL if not already set
+  // }
+  
+  console.log('Using API Base URL:', BASE_URL);
+  
   try {
     console.log('Trying to request from:', `${BASE_URL}/search`);
     
